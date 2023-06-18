ef661859fe82f19942b2145ae6b810221879c575
Ganesh N <ganeshnarvekar09@gmail.com>
2023-06-18 12:59:35 +0530

    Added a code to demonstrate Interface example

---
+++ classes-and-objects/src/inter/face/example/Manager.java
@@ -0,0 +1,8 @@
+package inter.face.example;
+
+public class Manager {
+	public void checkAttendence() {
+		System.out.println("I am playing a new responsbility in my TECHNICAL-LEAD way");
+		
+	}
+}
---
+++ classes-and-objects/src/inter/face/example/TechnicalLead.java
@@ -0,0 +1,10 @@
+package inter.face.example;
+
+public class TechnicalLead implements AdhocResponsibilities{
+
+	public void checkAttendence() {
+		System.out.println("I am playing a new responsbility in my TECHNICAL-LEAD way");
+		
+	}
+
+}
